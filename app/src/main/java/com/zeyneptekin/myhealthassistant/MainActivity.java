20fa5d015cd2fd730a13d67cff9e33e04c602a13
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-02-17 10:26:45 +0300

    Initial commit

---
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zeyneptekin.myhealthassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //2.35 te menu oluşturmada kaldım
+    }
+}
