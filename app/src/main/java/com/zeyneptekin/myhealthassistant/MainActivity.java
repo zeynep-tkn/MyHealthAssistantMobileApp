40ac79319d4516576d9b704bf15f981820f38580
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-02-18 11:28:59 +0300

    deneme 2

--- app/src/main/java/com/zeyneptekin/myhealthassistant/MainActivity.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //2.35 te menu oluşturmada kaldım
+        //menu olusturuluyor
     }
 }
