8a1f2f9f9c500dd63e668d30ef9a1a4bcaa33ea2
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-02-18 16:47:40 +0300

    Bottom Navigation Bar oluşturuldu

--- app/src/main/java/com/zeyneptekin/myhealthassistant/MainActivity.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/MainActivity.java
@@ -1,15 +1,46 @@
 package com.zeyneptekin.myhealthassistant;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.zeyneptekin.myhealthassistant.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+   ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //menu olusturuluyor
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new CategoriesFragment());
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+            int itemId=item.getItemId();
+
+            if (itemId==R.id.categories){
+                replaceFragment(new CategoriesFragment());
+            }
+            else if(itemId==R.id.home){
+                replaceFragment(new HomeFragment());
+            }
+            else if(itemId==R.id.profile){
+                replaceFragment(new ProfileFragment());
+            }
+             return true;
+        });
+
+
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout2,fragment);
+        fragmentTransaction.commit();
     }
 }
