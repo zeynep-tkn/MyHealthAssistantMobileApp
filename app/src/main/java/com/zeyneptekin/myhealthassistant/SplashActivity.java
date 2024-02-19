f5cfddcd1773445b78761298ff109314e42c59ab
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-02-19 11:04:34 +0300

    splash activity oluşturuldu

---
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.zeyneptekin.myhealthassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DISPLAY_LENGTH = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        // Splash ekranını belirli bir süre gösterdikten sonra ana ekrana geçiş yapılacak
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                // Ana ekrana geçiş yap
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish(); // Splash ekranını kapat
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
