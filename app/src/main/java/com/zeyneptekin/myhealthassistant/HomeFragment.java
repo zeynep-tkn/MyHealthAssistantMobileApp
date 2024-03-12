79df40aa984e81e8310f7a1c6e8e0c1079af904c
mervekul <mervekul1104@gmail.com>
2024-03-12 17:06:02 +0300

    test commit

--- app/src/main/java/com/zeyneptekin/myhealthassistant/HomeFragment.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/HomeFragment.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
-
+    //DENEME YORUMU
     CardView MotivationCard;
     CardView SportsCard;
     CardView EducationCard;
