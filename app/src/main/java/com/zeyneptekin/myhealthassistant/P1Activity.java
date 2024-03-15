c727b0e2754ac5744d49702f82eff32a96a60b66
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-03-15 15:02:07 +0300

    egzersiz sayfası oluşturuldu

---
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/P1Activity.java
@@ -0,0 +1,14 @@
+package com.zeyneptekin.myhealthassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class P1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p1);
+    }
+}
--- app/src/main/java/com/zeyneptekin/myhealthassistant/SportsExerciseActivity.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/SportsExerciseActivity.java
@@ -1,31 +1,77 @@
 package com.zeyneptekin.myhealthassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SportsExerciseActivity extends AppCompatActivity {
+    Button button1,button2,button3,button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sports_exercise);
+        Toolbar toolbar= findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        button1=findViewById(R.id.startP1);
+        button2=findViewById(R.id.startP2);
+        button3=findViewById(R.id.startP3);
+        button4=findViewById(R.id.startP4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SportsExerciseActivity.this, P1Activity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SportsExerciseActivity.this, P2Activity.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SportsExerciseActivity.this, P3Activity.class);
+                startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SportsExerciseActivity.this, P4Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+
     public void exerciseP1(View view) {
+     Intent intent =new Intent(SportsExerciseActivity.this, P1Activity.class);
+     startActivity(intent);
 
     }
 
     public void exerciseP2(View view) {
-
+        Intent intent =new Intent(SportsExerciseActivity.this, P2Activity.class);
+        startActivity(intent);
     }
 
     public void exerciseP3(View view) {
-
+        Intent intent =new Intent(SportsExerciseActivity.this, P3Activity.class);
+        startActivity(intent);
     }
 
     public void exerciseP4(View view) {
-
+        Intent intent =new Intent(SportsExerciseActivity.this, P4Activity.class);
+        startActivity(intent);
     }
 }
