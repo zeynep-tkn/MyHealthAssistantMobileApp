4b627e65a2c5264ce31a829605ee59af0475ae79
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-03-19 00:54:19 +0300

    spor ve egzersiz sayfası devam

--- app/src/main/java/com/zeyneptekin/myhealthassistant/P1Activity.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/P1Activity.java
@@ -3,6 +3,7 @@ package com.zeyneptekin.myhealthassistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class P1Activity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class P1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p1);
+    }
+
+    public void ImageButtonClicked(View view) {
+
     }
 }
