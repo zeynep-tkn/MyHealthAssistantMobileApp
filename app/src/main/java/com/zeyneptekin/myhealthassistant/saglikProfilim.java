f42ac050a93abb997e002a140725a95616241133
Beyzanur Bayır <bayirbeyzanur574@gmail.com>
2024-03-23 00:05:06 +0300

    saglik profilim sayfasi eklendi

---
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/saglikProfilim.java
@@ -0,0 +1,16 @@
+package com.zeyneptekin.myhealthassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class saglikProfilim extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saglik_profilim);
+    }
+}
+
+
