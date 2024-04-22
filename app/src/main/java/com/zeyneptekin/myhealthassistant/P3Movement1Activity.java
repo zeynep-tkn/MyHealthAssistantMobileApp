56d7ec9ed1a36915ead48daf467bda33bc08028e
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-04-22 19:16:26 +0300

    new start

---
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/P3Movement1Activity.java
@@ -0,0 +1,14 @@
+package com.zeyneptekin.myhealthassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class P3Movement1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p3_movement1);
+    }
+}
