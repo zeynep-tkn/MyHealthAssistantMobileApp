72ac9a903e23e8c33aa95acb368ffe3032a0905e
Esforper <dolanbayomerfaruk@gmail.com>
2024-04-30 17:01:13 +0300

    firestoreHelper class has added,now in 'ilaç bilgilerim' page, ilaç alerjilerim informations database

--- app/src/main/java/com/zeyneptekin/myhealthassistant/ilacBilgilerim.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/ilacBilgilerim.java
@@ -3,12 +3,39 @@ package com.zeyneptekin.myhealthassistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ilacBilgilerim extends AppCompatActivity {
+
+    private Button ilacEkleBtn;
+    private EditText alerjikIlacIsmiGirmeTxt;
+
+    private FirestoreHelper firestoreHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ilac_bilgilerim);
+
+        // FirestoreHelper sınıfını başlat
+        firestoreHelper = new FirestoreHelper(this);
+
+        // Gerekli bileşenleri bul
+        ilacEkleBtn = findViewById(R.id.ilacAlerjisiEkleBtn);
+        alerjikIlacIsmiGirmeTxt = findViewById(R.id.alerjikIlacIsmiGirmeTxt);
+
+        // Butona tıklanınca Firestore'a veri eklemek için onClickListener ekle
+        ilacEkleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // EditText'ten ilaç ismini al
+                String ilacIsmi = alerjikIlacIsmiGirmeTxt.getText().toString().trim();
+
+                // FirestoreHelper üzerinden ilaç alerjisi eklemeyi çağır
+                firestoreHelper.addIlacAlerjisi(ilacIsmi);
+            }
+        });
     }
 }
