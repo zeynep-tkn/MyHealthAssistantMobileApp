3d491ce75e125b5a78986e4903c8246af9ffc4b8
Beyzanur Bayır <bayirbeyzanur574@gmail.com>
2024-05-03 14:09:24 +0300

    randevularım sayfasının ver içinabanı baÅğlantısı yapıldı

--- app/src/main/java/com/zeyneptekin/myhealthassistant/FirestoreHelper.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/FirestoreHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
@@ -76,6 +77,36 @@ public class FirestoreHelper {
                     }
                 });
     }
+    public void addRandevuBilgisi(String bolum, String tarih, String saat, String doktor, String hastahane) {
+        Map<String, Object> randevuBilgisi = new HashMap<>();
+        randevuBilgisi.put("bolum", bolum);
+        randevuBilgisi.put("tarih", tarih);
+        randevuBilgisi.put("saat", saat);
+        randevuBilgisi.put("doktor", doktor);
+        randevuBilgisi.put("hastahane", hastahane);
 
+        // Firestore koleksiyonunu ve belirli bir belgeyi belirle
+        DocumentReference docref = db.collection("users").document("aXjqaM073S5UPEMiT2fu").
+                collection("HealthInformations").document("RbEvitDDUmvkeIO9EKHx").collection("randevularım").document();
+        // Belgeye veriyi ekle ve başarılı olduğunda bir onSuccessListener dinle
 
+        docref.set(randevuBilgisi)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Veri başarıyla eklendi
+                        Toast.makeText(context, "Randevu bilgileri başarıyla kaydedildi!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Veri ekleme başarısız oldu
+                        Toast.makeText(context, "Randevu bilgileri eklenirken hata oluştu: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
+
+
+
