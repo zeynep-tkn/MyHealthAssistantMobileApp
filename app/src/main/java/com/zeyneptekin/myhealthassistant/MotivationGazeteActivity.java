600ac295a1abdb6183b50de425c54154e1ea2e95
ahberr <berraozdogan70@gmail.com>
2024-05-06 21:20:59 +0300

    gazete halledildi

--- app/src/main/java/com/zeyneptekin/myhealthassistant/MotivationGazeteActivity.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/MotivationGazeteActivity.java
@@ -13,69 +13,58 @@ public class MotivationGazeteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motivation_gazete);
 
-        ImageButton imageButton4 = findViewById(R.id.im1);
+        ImageButton imageButton1 = findViewById(R.id.im1);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete1Activity.class);
+                startActivity(intent);
+            }
+        });
 
+        ImageButton imageButton2 = findViewById(R.id.im2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageButton3 = findViewById(R.id.im3);
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageButton4 = findViewById(R.id.im4);
         imageButton4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete1Activity.class);
+                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete4Activity.class);
                 startActivity(intent);
             }
         });
 
-        ImageButton imageButton5 = findViewById(R.id.im2);
-
-
+        ImageButton imageButton5 = findViewById(R.id.im5);
         imageButton5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete1Activity.class);
+                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete5Activity.class);
                 startActivity(intent);
             }
         });
 
-        ImageButton imageButton6 = findViewById(R.id.im3);
-
-
+        ImageButton imageButton6 = findViewById(R.id.im6);
         imageButton6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete1Activity.class);
+                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete6Activity.class);
                 startActivity(intent);
             }
         });
-        ImageButton imageButton7 = findViewById(R.id.im4);
-
-
-        imageButton7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete1Activity.class);
-                startActivity(intent);
-            }
-        });
-
-        ImageButton imageButton8 = findViewById(R.id.im5);
-
-
-        imageButton8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete1Activity.class);
-                startActivity(intent);
-            }
-        });
-
-        ImageButton imageButton9 = findViewById(R.id.im6);
-
-
-        imageButton9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MotivationGazeteActivity.this, Gazete1Activity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
