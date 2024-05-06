d0ff173d6beddf756157b776003864097717a1bd
mervekul <mervekul1104@gmail.com>
2024-05-06 16:30:02 +0300

    spor akil egitim

---
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/SporPlanlariActivity.java
@@ -0,0 +1,14 @@
+package com.zeyneptekin.myhealthassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SporPlanlariActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spor_planlari);
+    }
+}
