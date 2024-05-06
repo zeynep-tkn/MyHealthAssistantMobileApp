34d6bdfdfc70cb3c5ea9e0a3a723c1a11a73af81
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-05-06 11:24:58 +0300

    .

--- app/src/main/java/com/zeyneptekin/myhealthassistant/HomeFragment.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.zeyneptekin.myhealthassistant;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -10,6 +11,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -20,6 +25,9 @@ public class HomeFragment extends Fragment {
     private CardView EducationCard;
     private CardView FoodCard;
     private CalendarView calendarView;
+    ImageButton PlusButton;
+    ImageButton MinusButton;
+    private TextView AmountText;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -53,7 +61,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
+        AmountText = view.findViewById(R.id.amountTextView);
         // Takvim bileşenini bul
         calendarView = view.findViewById(R.id.calendarView);
 
@@ -65,6 +73,11 @@ public class HomeFragment extends Fragment {
         SportsCard = view.findViewById(R.id.sportsCard);
         EducationCard = view.findViewById(R.id.educationCard);
         FoodCard = view.findViewById(R.id.foodCard);
+
+        MinusButton=view.findViewById(R.id.minusButton);
+        PlusButton=view.findViewById(R.id.plusButton);
+
+
 
         MotivationCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,6 +110,47 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        PlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mevcut değeri al
+                System.out.println("Plus butonuna tıklandı");
+                String currentValueString = AmountText.getText().toString();
+                // String'i integer'a dönüştür
+                int currentValue = Integer.parseInt(currentValueString);
+                // Eğer mevcut değer 8'den küçükse, değeri bir artır
+                System.out.println("Değer 1 arttırıldı");
+                if (currentValue < 8) {
+                    int newValue = currentValue + 1;
+                    // Yeni değeri TextView'e yaz
+                    AmountText.setText(newValue + "/8 bardak");
+                    System.out.println(newValue+ "/8 bardak");
+                    // Eğer yeni değer 8 ise "Başarılı" mesajını göster
+                    if (newValue == 8) {
+                        Toast.makeText(getActivity(), "Başarılı", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        MinusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Minus butonuna tıklandı");
+                // Mevcut değeri al
+                String currentValueString = AmountText.getText().toString();
+                // String'i integer'a dönüştür
+                int currentValue = Integer.parseInt(currentValueString);
+                // Eğer mevcut değer 0'dan büyükse, değeri bir azalt
+                if (currentValue > 0) {
+                    int newValue = currentValue - 1;
+                    // Yeni değeri TextView'e yaz
+                    AmountText.setText(newValue + "/8 bardak");
+
+                }
+            }
+        });
+
 
         return view;
     }
