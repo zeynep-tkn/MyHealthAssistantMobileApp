9a46095d7c731e81fae59944fbaf2d57fe7fc6f0
zeynep <115029576+zeynep-tkn@users.noreply.github.com>
2024-05-09 14:21:03 +0300

    düzeltme

--- app/src/main/java/com/zeyneptekin/myhealthassistant/saglikliTarifler.java
+++ app/src/main/java/com/zeyneptekin/myhealthassistant/saglikliTarifler.java
@@ -9,8 +9,6 @@ import android.widget.LinearLayout;
 
 public class saglikliTarifler extends AppCompatActivity {
 
-   /*
-
     private LinearLayout SaglikliTarif1;
     private LinearLayout SaglikliTarif2;
     private LinearLayout SaglikliTarif3;
@@ -22,69 +20,44 @@ public class saglikliTarifler extends AppCompatActivity {
     private LinearLayout SaglikliTarif9;
     private LinearLayout SaglikliTarif10;
 
-
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saglikli_tarifler);
-
-
-        // LinearLayout'leri bul ve tıklama olayını ayarla
-        SaglikliTarif1 = findViewById(R.id.yesilBowlCard);
-        SaglikliTarif1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(saglikliTarifler.this, saglikliTarif2.class);
-                System.out.println("Sağlıklı tarifler1 sayfasına geçti");
-                startActivity(intent);
-            }
-        });
         @Override
         protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_saglikli_tarifler);
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_saglikli_tarifler);
 
+                // LinearLayout'leri bul ve tıklama olayını ayarla
+                SaglikliTarif1 = findViewById(R.id.yesilBowlCard);
+                SaglikliTarif1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Intent intent = new Intent(saglikliTarifler.this, saglikliTarif2.class);
+                                System.out.println("Sağlıklı tarifler1 sayfasına geçti");
+                                startActivity(intent);
+                        }
+                });
 
-            // LinearLayout'leri bul ve tıklama olayını ayarla
-            SaglikliTarif2 = findViewById(R.id.enerjiToplariCard);
-            SaglikliTarif2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(saglikliTarifler.this, yesilBowlTarifi.class);
-                    System.out.println("Sağlıklı tarifler2 sayfasına geçti");
-                    startActivity(intent);
-                }
-            });
+                // LinearLayout'leri bul ve tıklama olayını ayarla
+                SaglikliTarif2 = findViewById(R.id.enerjiToplariCard);
+                SaglikliTarif2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Intent intent = new Intent(saglikliTarifler.this, yesilBowlTarifi.class);
+                                System.out.println("Sağlıklı tarifler2 sayfasına geçti");
+                                startActivity(intent);
+                        }
+                });
 
-
-
-
-
-    }
-
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_saglikli_tarifler);
-
-
-            // LinearLayout'leri bul ve tıklama olayını ayarla
-            SaglikliTarif3 = findViewById(R.id.granolaCard);
-            SaglikliTarif3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(saglikliTarifler.this, saglikliTarif3.class);
-                    System.out.println("Sağlıklı tarifler3 sayfasına geçti");
-                    startActivity(intent);
-                }
-            }); */
-
-
-
+                // LinearLayout'leri bul ve tıklama olayını ayarla
+                SaglikliTarif3 = findViewById(R.id.granolaCard);
+                SaglikliTarif3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Intent intent = new Intent(saglikliTarifler.this, saglikliTarif3.class);
+                                System.out.println("Sağlıklı tarifler3 sayfasına geçti");
+                                startActivity(intent);
+                        }
+                });
 
 
         }
+}
